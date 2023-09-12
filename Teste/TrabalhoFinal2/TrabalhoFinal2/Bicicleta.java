4e20df0914de12b1574f0fbfe50acf2d43f8b098
Garniel <128650497+GabrielRibeiro1503@users.noreply.github.com>
2023-09-11 21:37:18 -0300

    Finalizado

--- Teste/TrabalhoFinal2/TrabalhoFinal2/Bicicleta.java
+++ Teste/TrabalhoFinal2/TrabalhoFinal2/Bicicleta.java
@@ -1,16 +1,18 @@
 package TrabalhoFinal2;
+
 import java.util.Random;
 
-public class Bicicleta extends Transporte{
-    
+public class Bicicleta extends Transporte {
+
     Random rand = new Random();
     private int tamAro;
-    private String amortecedor; 
+    private String amortecedor;
     private int qtdMarcha;
     private String nome;
     private int corrida;
 
-    public Bicicleta(int velocidadeMax, String combustivel, float qtdCombustivel, int tamAro, String amortecedor, int qtdMarcha, String nome){
+    public Bicicleta(int velocidadeMax, String combustivel, float qtdCombustivel, int tamAro, String amortecedor,
+            int qtdMarcha, String nome) {
         super(velocidadeMax, combustivel, qtdCombustivel);
         this.tamAro = tamAro;
         this.amortecedor = amortecedor;
@@ -18,76 +20,76 @@ public class Bicicleta extends Transporte{
         this.nome = nome;
     }
 
-    public int getTam_aro(){
+    public int getTam_aro() {
         return tamAro;
     }
 
-    public void setTam_aro(int tamAro){
+    public void setTam_aro(int tamAro) {
         this.tamAro = tamAro;
     }
 
-    public String getAmortecedor(){
+    public String getAmortecedor() {
         return amortecedor;
     }
 
-    public void setAmortecedor(String amortecedor){
+    public void setAmortecedor(String amortecedor) {
         this.amortecedor = amortecedor;
     }
 
-    public int getQtdMarcha(){
+    public int getQtdMarcha() {
         return qtdMarcha;
     }
 
-    public void setQtdMarcha(int qtdMarcha){
+    public void setQtdMarcha(int qtdMarcha) {
         this.qtdMarcha = qtdMarcha;
     }
 
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome){
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public void info(){
+    public void info() {
         super.info();
-        System.out.println("|| Tamanho Aro: "+this.tamAro);
-        System.out.println("|| Amortecedor: "+this.amortecedor);
-        System.out.println("|| Quantidade de Marcha: "+this.qtdMarcha);
-        System.out.println("|| Nome: "+this.nome);
+        System.out.println("|| Tamanho Aro: " + this.tamAro);
+        System.out.println("|| Amortecedor: " + this.amortecedor);
+        System.out.println("|| Quantidade de Marcha: " + this.qtdMarcha);
+        System.out.println("|| Nome: " + this.nome);
     }
 
     @Override
-    public void entrar(){
-        System.out.println("|| !! Você subiu na "+this.nome+" !!");
+    public void entrar() {
+        System.out.println("|| !! Você subiu na " + this.nome + " !!");
     }
 
     @Override
-    public void sair(){
+    public void sair() {
         System.out.println("||=======================================|");
-        System.out.println("|| !! Você desceu da "+this.nome+" !!");
+        System.out.println("|| !! Você desceu da " + this.nome + " !!");
         System.out.println("|| !! Obrigado por usar esse programa !!");
         System.out.println("||=======================================|");
     }
 
     @Override
-    public void bater(){
+    public void bater() {
         System.out.println("|| Você bateu a bicicleta e morreu !!");
     }
 
     @Override
-    public void corrida(){
+    public void corrida() {
         corrida = rand.nextInt(3);
 
-        if(corrida == 0){
+        if (corrida == 0) {
             System.out.println("||==============================|");
             System.out.println("|| Você pedalou mais que ele !!");
             System.out.println("|| Parabéns !!");
-        } else if(corrida == 1){
+        } else if (corrida == 1) {
             System.out.println("||==============================================|");
             System.out.println("|| Infelizmente ele foi mais rápido que você !!");
-            System.out.println("|| Você perdeu !!"); 
+            System.out.println("|| Você perdeu !!");
         } else {
             System.out.println("||========================|");
             System.out.println("|| Os dois são velozes !!");
--- Teste/TrabalhoFinal2/TrabalhoFinal2/Carro.java
+++ Teste/TrabalhoFinal2/TrabalhoFinal2/Carro.java
@@ -1,77 +1,78 @@
 package TrabalhoFinal2;
+
 import java.util.Random;
 
-public class Carro extends Transporte{
-    
+public class Carro extends Transporte {
+
     Random rand = new Random();
     private String marca;
     private String modelo;
     private int corrida;
 
-    public Carro(int velocidadeMax, String combustivel, float qtdCombustivel, String marca, String modelo){
+    public Carro(int velocidadeMax, String combustivel, float qtdCombustivel, String marca, String modelo) {
         super(velocidadeMax, combustivel, qtdCombustivel);
         this.marca = marca;
         this.modelo = modelo;
     }
 
-    public String getMarca(){
+    public String getMarca() {
         return marca;
     }
 
-    public void setMarca(String marca){
+    public void setMarca(String marca) {
         this.marca = marca;
     }
 
-    public String getModelo(){
+    public String getModelo() {
         return modelo;
     }
 
-    public void setModelo(String modelo){
+    public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
-    public void info(){
+    public void info() {
         super.info();
-        System.out.println("|| Marca: "+this.marca);
-        System.out.println("|| Modelo: "+this.modelo);
+        System.out.println("|| Marca: " + this.marca);
+        System.out.println("|| Modelo: " + this.modelo);
     }
 
-    public void abastecer(){
+    public void abastecer() {
         System.out.println("|| Você encheu o tanque !!");
         System.out.println("|| Vamos seguir viagem !!");
     }
 
     @Override
-    public void entrar(){
-        System.out.println("|| !! Você entrou no "+marca+" "+modelo+" !!");
+    public void entrar() {
+        System.out.println("|| !! Você entrou no " + marca + " " + modelo + " !!");
     }
 
     @Override
-    public void sair(){
+    public void sair() {
         System.out.println("||=========================================|");
-        System.out.println("|| !! Você saiu do "+marca+" "+modelo+" !!");
+        System.out.println("|| !! Você saiu do " + marca + " " + modelo + " !!");
         System.out.println("|| !! Obrigado por usar esse programa !!");
         System.out.println("||=========================================|");
     }
 
     @Override
-    public void bater(){
-            System.out.println("|| Você bateu o carro no trajeto e morreu !!");
-            System.out.println("||===========================================|");
+    public void bater() {
+        System.out.println("|| Você bateu o carro no trajeto e morreu !!");
+        System.out.println("||===========================================|");
     }
 
     @Override
-    public void corrida(){
+    public void corrida() {
         corrida = rand.nextInt(3);
 
-        if(corrida == 0){
+        if (corrida == 0) {
             System.out.println("||==================================|");
             System.out.println("|| O seu carro ganhou esse racha !!");
             System.out.println("|| Parabéns !!");
-        } else if(corrida == 1){
+        } else if (corrida == 1) {
             System.out.println("||=============================================|");
             System.out.println("|| Infelizmente ele foi mais rápido que você !!");
-            System.out.println("|| Você perdeu !!"); 
+            System.out.println("|| Você perdeu !!");
         } else {
             System.out.println("||========================|");
             System.out.println("|| Os dois são velozes !!");
