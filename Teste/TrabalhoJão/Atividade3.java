6c410812b1914ab2001b037e4e2be876df9b14a7
pc unifil <128650497+GabrielRibeiro1503@users.noreply.github.com>
2023-09-14 17:45:52 -0300

    Atividade

---
+++ Teste/TrabalhoJão/Atividade3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Atividade3{
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int W, X, Y, Z, diaInicio, horaInicio, minutoInicio, segundoInicio, diaFinal, horaFinal, minutoFinal, segundoFinal;
+        String horarioInicio, horarioFinal;
+        String[] partes;
+
+        System.out.print("Dia ");
+        diaInicio = scanner.nextInt();
+        System.out.print("Informe o horário (hh : mm : ss): ");
+        horarioInicio = scanner.next();
+
+        partes = horarioInicio.split(":");
+
+        horaInicio = Integer.parseInt(partes[0].trim());
+        minutoInicio = Integer.parseInt(partes[1].trim());
+        segundoInicio = Integer.parseInt(partes[2].trim());
+
+        System.out.print("Dia ");
+        diaFinal = scanner.nextInt();
+        System.out.print("Informe o horário (hh : mm : ss): ");
+        horarioFinal = scanner.nextLine();
+
+        partes = horarioFinal.split(":");
+
+        horaFinal = Integer.parseInt(partes[0].trim());
+        minutoFinal = Integer.parseInt(partes[1].trim());
+        segundoFinal = Integer.parseInt(partes[2].trim());
+
+        
+
+    }
+}
+
