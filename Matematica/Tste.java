73c76c991c1e658c63bd699cc5dce9f67945ac77
pc unifil <>
2023-11-16 17:20:07 -0300

    RSA

---
+++ Matematica/Tste.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Tste{
+    public static void main(String[] args){
+       
+        Scanner scan = new Scanner(System.in);
+        int n;
+        int p = 10333; // tem que ser um número primo random
+        int q = 1031; // tem que ser um número primo random
+        int totiente;
+
+        n = p * q;
+
+        totiente = (p - 1) * (q - 1);
+
+
+    }
+}
